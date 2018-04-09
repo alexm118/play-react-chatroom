764825bb43eee295a1bda13c0dd644a792e58a20
Alex Martin <alexander.martin@excella.com>
2018-04-08 23:29:21 -0400

    Basic Room Implementation, need to refactor, and restructure

--- app/controllers/MessageController.scala
+++ app/controllers/MessageController.scala
@@ -1,17 +1,26 @@
 package controllers
 
+import java.util.concurrent.TimeUnit
 import javax.inject.Inject
 import javax.inject.Singleton
 
-import akka.actor.ActorSystem
+import actors.{ChatActor, Room, RoomActor}
+import akka.actor.{ActorRef, ActorSystem}
 import akka.stream.Materializer
 import akka.stream.scaladsl.{BroadcastHub, Flow, Keep, MergeHub, Source}
 import play.api.libs.json.JsValue
+import play.api.libs.streams.ActorFlow
 import play.api.mvc.{AbstractController, ControllerComponents, WebSocket}
 
+import scala.concurrent.{Await, ExecutionContext}
+import scala.concurrent.duration.{Duration, FiniteDuration}
+import scala.util.{Failure, Success}
+
+
 @Singleton
-class MessageController @Inject()(cc: ControllerComponents)
-                                 (implicit system: ActorSystem, mat: Materializer) extends AbstractController(cc) {
+class MessageController @Inject()(cc: ControllerComponents, room: Room)
+                                 (implicit system: ActorSystem,
+                                  mat: Materializer, executionContext: ExecutionContext) extends AbstractController(cc) {
 
   private val (chatSink, chatSource) = {
 
@@ -22,9 +31,28 @@ class MessageController @Inject()(cc: ControllerComponents)
     source.toMat(sink)(Keep.both).run()
   }
 
-  private val userFlow: Flow[JsValue, JsValue, _] = {
+  private val chatFlow: Flow[JsValue, JsValue, _] = {
     Flow[JsValue].via(Flow.fromSinkAndSource(chatSink, chatSource))
   }
 
-  def message: WebSocket = WebSocket.accept[JsValue, JsValue] { _ => userFlow }
+
+  def message: WebSocket = WebSocket.accept[JsValue, JsValue] { _ => chatFlow }
+
+  def room(name: String): WebSocket = WebSocket.accept[JsValue, JsValue] { _ => {
+    var roomActor: ActorRef = null
+    try {
+      roomActor = system.actorOf(RoomActor.props, name)
+    } catch {
+      case _ => val future = system.actorSelection(s"user/$name").resolveOne(FiniteDuration.apply(5, TimeUnit.SECONDS))
+        future.onComplete {
+          case Success(actor) => roomActor = actor
+          case Failure(_) => roomActor = system.actorOf(RoomActor.props, name)
+        }
+        Await.result(future, Duration.Inf)
+    }
+
+    ActorFlow.actorRef(out => ChatActor.props(out, roomActor))
+  }
+  }
+
 }
